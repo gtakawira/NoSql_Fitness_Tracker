151bd8887672c57fffe890c0f8f50a4f541edd59
blacxcellence <tgtakawira@gmail.com>
2021-11-02 01:26:20 -0500

    added api routes

--- controllers/api/api.js
+++ controllers/api/api.js
@@ -1,36 +1,96 @@
 const router = require("express").Router();
-const {Workout} = require("../../models/workout.js");
+const {Workout} = require("../../models");
 
 // router.post("/api/workout", ({ body }, res) => {
 //   Workout.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
+//     .then(dbworkout => {
+//       res.json(dbworkout);
 //     })
 //     .catch(err => {
 //       res.status(400).json(err);
 //     });
 // });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.post("/", async ({ body }, res) => {
+  Workout.create(body)
+    .then(dbworkout => {
+      res.json(dbworkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.get("workouts", (req, res) => {
-  Workout.find({})
-    // .sort({ date: -1 })
-    console.log("get Workouts")
-    .then(dbworkouts => {
-      res.json(dbworkouts);
+router.get("/", async  (req, res) => {
+  Workout.find({}).then(dbworkouts => {res.json(dbworkouts);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
+
+
+
+//PUT an excercise (update)
+//exercise exists inside a workout so update workout
+router.put("/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: {
+        excercise: req.body,
+      },
+    }
+  )
+    .then((workoutdb) => {
+      res.json(workoutdb);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+//DELETE an excercise
+router.delete("/:id", (req, res) => {
+  Workout.findByIdAndDelete(
+    {
+      _id: req.params.id,
+    },
+    {
+      remove: {
+        excercise: req.body,
+      },
+    }
+  )
+    .then((workoutdb) => {
+      res.json(workoutdb);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/workouts/range", (req, res) => {
+    console.log("You hit duration!");
+  //all purpose tool for data manipulation
+  Workout.aggregate([
+    {
+      $addFields: {
+        durationTotal: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((getWorkout) => {
+      res.json(getWorkout);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+
 
 module.exports = router;
