5e1bd275681e7aad6fe96c9f1c84c2069d94c731
blacxcellence <tgtakawira@gmail.com>
2021-11-01 22:57:11 -0500

    added the schema

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  exercise: [
+    {
+      type: {
+    type: String,
+    trim: true,
+    required: "What is the type of exercise"
+  },
+  name: {
+    type: String,
+    trim: true,
+        required: "Enter a name of the exercise"
+  },
+  duration: {
+    type: Number,
+    trim: true,
+    required: "Duration of exercise"
+  },
+ 
+  weight: {
+    type: Number,
+    trim: true,
+    required: "Enter the weight"
+  },
+  reps: {
+    type:Number,
+    trim: true,
+    required: "Number of Reps"
+  },
+  sets:{
+    type:Number,
+    trim: true,
+    required: "Number of Sets"
+
+  }
+}]});
+
+const Workout = mongoose.model("Workout", exerciseSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Transaction = require("../models/exercise.js");
+
+module.exports = router;
